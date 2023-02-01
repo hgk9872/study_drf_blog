5800e15a3521d786c4c5d06eabaaca0c0b81fde2
hugo <hgk9872@gmail.com>
2023-02-02 03:01:35 +0900

    PostSerializer 구현

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,8 +1,18 @@
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(ModelSerializer):
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'username']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    # username = serializers.ReadOnlyField(source='author.username')
+    author = AuthorSerializer()
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['pk', 'content', 'created_at', 'updated_at', 'author']
