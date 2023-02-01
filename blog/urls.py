5b6a82742445da0a5639bd8d3610fdb358bca706
hugo <hgk9872@gmail.com>
2023-02-01 20:35:10 +0900

    Serializer 추가

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('post', views.PostViewSet)  # 2개의 url 패턴 생성
+# router.urls  # url pattern 리스트
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
