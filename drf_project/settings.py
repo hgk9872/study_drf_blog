5e15ae914986c49a1516a7778b701b0110fe5128
hugo <hgk9872@gmail.com>
2023-02-03 18:17:19 +0900

    페이징 처리

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -120,6 +120,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'PAGE_SIZE': 3,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
